713552d5e8075071f159c976fd9e031152944101
joshmiller04 <mill3643@msu.edu>
2025-02-27 18:38:20 +0000

    new

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,10 +1,9 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("js"); // Copy js folder to _site/js
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("js"); // Ensure js folder is copied to _site/js
 
   return {
       dir: {
           input: ".",          // Root directory as input
-          includes: "_includes",
           output: "_site"      // Output folder for the built site
       }
   };
