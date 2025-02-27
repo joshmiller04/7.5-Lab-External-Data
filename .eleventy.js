1f5ed3d02a6cd4caee56652e99b6b80e27873f9b
joshmiller04 <mill3643@msu.edu>
2025-02-27 18:34:12 +0000

    oops

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,11 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("styles");
-  eleventyConfig.addPassthroughCopy("Imgs");
+  eleventyConfig.addPassthroughCopy("js"); // Copy js folder to _site/js
 
   return {
       dir: {
-          input: ".",
+          input: ".",          // Root directory as input
           includes: "_includes",
-          output: "_site"
+          output: "_site"      // Output folder for the built site
       }
   };
 };
